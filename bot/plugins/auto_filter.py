f9de85c89f73516158b7b9a83373a1d7078d0bab
Leechbot111 <104361832+Leechbot111@users.noreply.github.com>
2022-04-26 19:31:49 +0200

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("⚡️ Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("🌍 Next ⏩", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
